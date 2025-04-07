6c7294c10bae1c660221ac8d60a907df54e5b0d8
Yangnzzzz <yang_nianzi@126.com>
2025-04-07 06:58:29 +0000

    add new function

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import datetime
 import google.generativeai as genai
 import os
 import wikipedia
+import requests
 
 api = os.getenv("makersuite")
+telegram_api = os.getenv("telegram")
 model = genai.GenerativeModel("gemini-1.5-flash")
 genai.configure(api_key=api)
 
@@ -44,6 +46,45 @@ def foodexp_pred():
 @app.route("/foodexp1",methods=["POST","GET"])
 def foodexp1():
     return(render_template("foodexp1.html"))
+
+@app.route("/telegram",methods=["POST","GET"])
+def telegram():
+    return(render_template("telegram.html"))
+
+@app.route("/interest_pred",methods=["POST","GET"])
+def interest_pred():
+    q = float(request.form.get("q"))
+    url = f"https://api.telegram.org/bot{telegram_api}/"
+    updates = url+"getUpdates"
+    r = requests.get(updates)
+    r = r.json()
+    print(r)
+    chatId = r["result"][-1]["message"]["chat"]["id"]
+    prompt = "please enter inflation rate, type exit to break"
+    msg = url + f"sendMessage?chat_id={chatId}&text={prompt}"
+    requests.get(msg)
+    q = "The predicted interest rate is" + str(q+1.5)
+    msg = url + f"sendMessage?chat_id={chatId}&text={q}"
+    requests.get(msg)
+    return(render_template("telegram_reply.html",r=q))
+
+@app.route('/investment', methods=['POST'])
+def investment():
+    investment_type = request.form.get('type')
+    if investment_type == "Equity":
+        txt = "Equity" 
+    elif investment_type == "Fixed Income":
+        txt = "Fixed Income" 
+    elif investment_type == "Alternatives":
+        txt = "Alternatives" 
+    prompt = f"Please provide top 3 investment recommendations in {txt}.List the recommendations in the following strict format:1. Recommendation One 2. Recommendation Two 3. Recommendation Three .Do not include any introductory or concluding text. Just the raw list in the specified format."
+    r = model.generate_content(prompt)
+    print(r)
+    return render_template("investment_recom.html", r=r.candidates[0].content.parts[0].text,investment_type=investment_type)
+
+@app.route("/investment_recom",methods=["POST","GET"])
+def investment_recom():
+    return(render_template("asset_choice.html"))
 
 @app.route("/ethical_test",methods=["POST","GET"])
 def ethical_test():
